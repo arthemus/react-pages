4515c700f30f12d3d3153d1872bba97d93c4c500
Arthemus <arthemus.moreira@gmail.com>
2019-10-23 14:41:34 +1100

    Updates

---
+++ precache-manifest.ab805432c1a3be2018140510990108fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5529634bc899ea949b6a117f896de6",
+    "url": "/react-pages/index.html"
+  },
+  {
+    "revision": "f458f29e28d9d943b1ad",
+    "url": "/react-pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "dbaec3f5b86bd9477e49",
+    "url": "/react-pages/static/js/2.0cc35389.chunk.js"
+  },
+  {
+    "revision": "f458f29e28d9d943b1ad",
+    "url": "/react-pages/static/js/main.892c8a8d.chunk.js"
+  },
+  {
+    "revision": "9b1bdd285353174c0f2e",
+    "url": "/react-pages/static/js/runtime-main.50d60c8e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-pages/static/media/logo.25bf045c.svg"
+  }
+]);
